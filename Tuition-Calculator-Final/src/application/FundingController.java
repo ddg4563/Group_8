0c4f3985177bc7bc7e11eed929dbab48adf630a3
ddg4563 <daledgraham@gmail.com>
2020-04-10 21:20:51 -0500

    Added webview

---
+++ Tuition-Calculator-Final/src/application/FundingController.java
@@ -0,0 +1,26 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+public class FundingController {
+
+    @FXML
+    private WebView myView;
+
+    @FXML
+    private Button button1;
+    
+    private WebEngine engine;
+    
+    public void init() {
+    	System.out.println("fired");
+    	this.engine = this.myView.getEngine();
+    	this.engine.load("https://www.google.com");
+    	this.myView.setZoom(0.8);
+    }
+
+
+}
